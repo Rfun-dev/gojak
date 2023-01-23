a575c1513f443a248535a8b5fac1dd377e80510a
Rfun-dev <erfan.ferdianto2003@gmail.com>
2023-01-23 22:38:35 +0700

    hai asli beres serius

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className='w-full flex fixed flex-wrap items-center justify-center px-2 bg-hitam mb-3 text-white font-sans z-10'>
         <div className='container mx-auto flex flex-wrap items-center justify-between px-24'>
             <a href='/' className='flex items-center justify-center'>
-                <img src='/images/icon2.png?' className='h-28 w-16' alt="icon"/>
+                <img src='/images/icon2.png' className='h-28 w-16' alt="icon"/>
                 <span className='text-xl leading-relaxed inline-block mr-4 py-2 uppercase font-bold -m-3'>Gojek</span>
             </a>
             <div>
