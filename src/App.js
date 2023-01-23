54c54d4c9dad9c7756e44b14b185467be6e0a08d
Rfun-dev <erfan.ferdianto2003@gmail.com>
2023-01-23 23:38:40 +0700

    finish asli bener gak bohong 2 rius ajir lah

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 
 import Home from "./page/Home";
 import Careers from "./page/Careers";
@@ -9,15 +9,15 @@ import Profile from "./page/Profile";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="careers" element={<Careers />} />
-        <Route path="product" element={<Product />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="profile" element={<Profile />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
